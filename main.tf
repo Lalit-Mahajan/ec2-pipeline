b27b6c70cb9ed4db3b5b49b53a1463e55345c9df
Lalit Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2024-10-24 14:27:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ region = "ap-south-1"
 resources "aws_instance" "one" {
 ami = "ami-0e0e417dfa2028266"
 instance_type = "t2.micro"
-vpc_security_group_ids = ["sg-0720aceb2a61f908e"]
 tags = {
 Name = "jenkins-pipeline"
 }
